3eb987eca150582091a1c1aefa23d60ddd678c92
yosef salmalian <flaslnm@gmail.com>
2020-07-30 00:01:06 +0430

    webpack init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import _ from 'lodash'
+
+function component() {
+    const element = document.createElement('div')
+    element.innerHTML = 'hamed is cool'
+    return element
+}
+document.body.appendChild(component())
